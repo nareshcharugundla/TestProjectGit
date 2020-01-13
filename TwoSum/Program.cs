c3e914ff67fc50b89fa4f2e8be5d1677f95c784f
Naresh Charugundla <nachar@microsoft.com>
2020-01-13 15:09:14 -0800

    commiting test Project to Git

---
+++ TwoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 4, 7, 1, 9, 5 };
+            int target = 8;
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                if((a[i]+a[i+1]) == target)
+                {
+                    Console.WriteLine($"The two indices are {i}{i++}");
+                }
+            }
+
+        }
+    }
+}
